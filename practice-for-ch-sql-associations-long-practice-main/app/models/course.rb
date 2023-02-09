6c77634e3c26caa1c0e76010ca967dc209d401b6
Steven Paalz <stevenpaalz@gmail.com>
2023-02-09 14:57:37 -0500

    user and enrollment associations

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -1,2 +1,16 @@
 class Course < ApplicationRecord
+
+    has_many(
+        :enrollment,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: 'Enrollment'
+    )
+
+    # belongs_to(
+    #     :instructor,
+    #     primary_key: id,
+        
+    # )
+
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+    has_many(
+        :enrollment,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: 'Enrollment'
+    )
 end
